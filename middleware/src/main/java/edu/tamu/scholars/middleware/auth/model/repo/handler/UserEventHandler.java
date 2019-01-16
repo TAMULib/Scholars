43daa1e6ea60d23c0fb575f3e22c42d44105714f
William Welling <wwelling@library.tamu.edu>
2019-01-15 23:53:39 -0600

    users are never created via spring data rest

--- middleware/src/main/java/edu/tamu/scholars/middleware/auth/model/repo/handler/UserEventHandler.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/auth/model/repo/handler/UserEventHandler.java
@@ -1,14 +1,12 @@
 package edu.tamu.scholars.middleware.auth.model.repo.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleAfterDelete;
 import org.springframework.data.rest.core.annotation.HandleAfterSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import edu.tamu.scholars.middleware.auth.model.User;
-import edu.tamu.scholars.middleware.messaging.CreateEntityMessage;
 import edu.tamu.scholars.middleware.messaging.DeleteEntityMessage;
 import edu.tamu.scholars.middleware.messaging.UpdateEntityMessage;
 
@@ -17,12 +15,6 @@ public class UserEventHandler {
 
     @Autowired
     private SimpMessagingTemplate simpMessageTemplate;
-
-    @HandleAfterCreate
-    public void broadcastUserCreate(User user) {
-        simpMessageTemplate.convertAndSend(channel(), new CreateEntityMessage<User>(user));
-        simpMessageTemplate.convertAndSendToUser(user.getEmail(), channel(), new CreateEntityMessage<User>(user));
-    }
 
     @HandleAfterSave
     public void broadcastUserUpdate(User user) {
