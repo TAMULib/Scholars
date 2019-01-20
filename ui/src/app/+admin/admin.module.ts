0d55d6283e95b12a26f9021adb5d1b206d8f6cee
William Welling <wwelling@library.tamu.edu>
2019-01-20 09:20:46 -0600

    initial admin module translations

--- ui/src/app/+admin/admin.module.ts
+++ ui/src/app/+admin/admin.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MissingTranslationHandler, TranslateModule } from '@ngx-translate/core';
 
 import { SharedModule } from '../shared/shared.module';
+
+import { CustomMissingTranslationHandler } from '../core/handler/custom-missing-translation.handler';
+
 import { AdminComponent } from './admin.component';
 import { ThemesComponent } from './themes/themes.component';
 import { UsersComponent } from './users/users.component';
@@ -18,6 +22,13 @@ import { routes } from './admin.routes';
     imports: [
         CommonModule,
         SharedModule,
+        TranslateModule.forChild({
+            missingTranslationHandler: {
+                provide: MissingTranslationHandler,
+                useClass: CustomMissingTranslationHandler
+            },
+            isolate: false
+        }),
         RouterModule.forChild(routes)
     ]
 })
