09fac4089e0ca310a1fe6b9ff4e5f0dc48159ce8
William Welling <wwelling@library.tamu.edu>
2019-02-02 17:23:36 -0600

    some refactoring and organization

--- middleware/src/main/java/edu/tamu/scholars/middleware/discovery/model/Publication.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/discovery/model/Publication.java
@@ -17,31 +17,34 @@ import edu.tamu.scholars.middleware.harvest.annotation.Source;
 //@formatter:off
 @Source(
     key = "publication.class",
-    properties = {
-        @Source.Property(name = "abstractText", key = "publication.abstract"),
-        @Source.Property(name = "bookTitleForChapter", key = "publication.bookTitleForChapter"),
-        @Source.Property(name = "doi", key = "publication.doi"),
-        @Source.Property(name = "freetextKeyword", key = "publication.freetextKeyword"),
-        @Source.Property(name = "fullAuthorList", key = "publication.fullAuthorList"),
-        @Source.Property(name = "fullEditorList", key = "publication.fullEditorList"),
-        @Source.Property(name = "identifier", key = "publication.identifier"),
-        @Source.Property(name = "isbn10", key = "publication.isbn10"),
-        @Source.Property(name = "isbn13", key = "publication.isbn13"),
-        @Source.Property(name = "issn", key = "publication.issn"),
-        @Source.Property(name = "issue", key = "publication.issue"),
-        @Source.Property(name = "pcmid", key = "publication.pcmid"),
-        @Source.Property(name = "title", key = "publication.title.value", schema = "publication.title.schema", parse = false),
-        @Source.Property(name = "type", key = "publication.type.value", schema = "publication.type.schema", parse = true),
-        @Source.Property(name = "pageEnd", key = "publication.pageEnd"),
-        @Source.Property(name = "pageStart", key = "publication.pageStart"),
-        @Source.Property(name = "uri", key = "publication.uri"),
-        @Source.Property(name = "volume", key = "publication.volume"),
-    },
     sparql = {
-        @Source.Sparql(
-            template = "publication/authorships",
+		@Source.Sparql(
+            template = "publication/publication",
             properties = {
-                @Source.Property(name = "authorships", key = "publication.authorships.title", id = "authorshipIds")
+                @Source.Property(name = "abstractText", key = "publication.abstract"),
+                @Source.Property(name = "bookTitleForChapter", key = "publication.bookTitleForChapter"),
+                @Source.Property(name = "doi", key = "publication.doi"),
+                @Source.Property(name = "freetextKeyword", key = "publication.freetextKeyword"),
+                @Source.Property(name = "fullAuthorList", key = "publication.fullAuthorList"),
+                @Source.Property(name = "fullEditorList", key = "publication.fullEditorList"),
+                @Source.Property(name = "identifier", key = "publication.identifier"),
+                @Source.Property(name = "isbn10", key = "publication.isbn10"),
+                @Source.Property(name = "isbn13", key = "publication.isbn13"),
+                @Source.Property(name = "issn", key = "publication.issn"),
+                @Source.Property(name = "issue", key = "publication.issue"),
+                @Source.Property(name = "pcmid", key = "publication.pcmid"),
+                @Source.Property(name = "title", key = "publication.title.value", schema = "publication.title.schema", parse = false),
+                @Source.Property(name = "type", key = "publication.type.value", schema = "publication.type.schema", parse = true),
+                @Source.Property(name = "pageEnd", key = "publication.pageEnd"),
+                @Source.Property(name = "pageStart", key = "publication.pageStart"),
+                @Source.Property(name = "uri", key = "publication.uri"),
+                @Source.Property(name = "volume", key = "publication.volume"),
+            }
+        ),
+        @Source.Sparql(
+            template = "publication/authors",
+            properties = {
+                @Source.Property(name = "authors", key = "publication.authors.title", id = "authorIds")
             }
         ),
         @Source.Sparql(
@@ -86,48 +89,48 @@ import edu.tamu.scholars.middleware.harvest.annotation.Source;
 @SolrDocument(collection = "publications")
 public class Publication extends AbstractSolrDocument {
 
+    @Indexed
+    private String type;
+
+    @Indexed
+    private String identifier;
+
+    @Indexed
+    private String doi;
+
+    @Indexed
+    private String isbn10;
+
+    @Indexed
+    private String isbn13;
+
+    @Indexed
+    private String issn;
+
+    @Indexed
+    private String issue;
+
+    @Indexed
+    private String pcmid;
+
+    @Indexed
+    private String title;
+
     @Field(value = "abstract")
     @Indexed(name = "abstract")
     private String abstractText;
 
     @Indexed
-    private List<String> authorships;
+    private String pageEnd;
 
     @Indexed
-    private List<String> authorshipIds;
+    private String pageStart;
+
+    @Indexed
+    private String volume;
 
     @Indexed
     private String bookTitleForChapter;
-
-    @Indexed
-    private List<String> contacts;
-
-    @Indexed
-    private List<String> contactEmails;
-
-    @Indexed
-    private List<String> contactIds;
-
-    @Indexed
-    private List<String> contactPhones;
-
-    @Indexed
-    private List<String> etdChairs;
-
-    @Indexed
-    private List<String> etdChairEmails;
-
-    @Indexed
-    private List<String> etdChairIds;
-
-    @Indexed
-    private List<String> etdChairPhones;
-
-    @Indexed(type = "pdate")
-    private List<String> dateTimeValue;
-
-    @Indexed
-    private String doi;
 
     @Indexed
     private String freetextKeyword;
@@ -139,25 +142,58 @@ public class Publication extends AbstractSolrDocument {
     private String fullEditorList;
 
     @Indexed
+    private String uri;
+
+    @Indexed(type = "pdate")
+    private String datePublished;
+
+    @Indexed
+    private List<String> authors;
+
+    @Indexed
+    private List<String> authorIds;
+
+    @Indexed
+    private List<String> publishers;
+
+    @Indexed
+    private List<String> publisherEmails;
+
+    @Indexed
+    private List<String> publisherPhones;
+
+    @Indexed
+    private List<String> publisherIds;
+
+    @Indexed
+    private List<String> etdChairs;
+
+    @Indexed
+    private List<String> etdChairEmails;
+
+    @Indexed
+    private List<String> etdChairPhones;
+
+    @Indexed
+    private List<String> etdChairIds;
+    
+    @Indexed
+    private List<String> contacts;
+
+    @Indexed
+    private List<String> contactEmails;
+
+    @Indexed
+    private List<String> contactPhones;
+
+    @Indexed
+    private List<String> contactIds;
+
+    @Indexed
     private List<String> grant;
 
     @Indexed
     private List<String> grantIds;
-
-    @Indexed
-    private String identifier;
-
-    @Indexed
-    private String isbn10;
-
-    @Indexed
-    private String isbn13;
-
-    @Indexed
-    private String issn;
-
-    @Indexed(type = "string")
-    private String issue;
 
     @Indexed
     private List<String> journal;
@@ -167,33 +203,6 @@ public class Publication extends AbstractSolrDocument {
 
     @Indexed
     private List<String> journalIssn;
-
-    @Indexed
-    private String pcmid;
-
-    @Indexed
-    private String title;
-
-    @Indexed
-    private String type;
-
-    @Indexed(type = "string")
-    private String pageEnd;
-
-    @Indexed(type = "string")
-    private String pageStart;
-
-    @Indexed
-    private List<String> publishers;
-
-    @Indexed
-    private List<String> publisherEmails;
-
-    @Indexed
-    private List<String> publisherIds;
-
-    @Indexed
-    private List<String> publisherPhones;
 
     @Indexed
     private List<String> subjectAreas;
@@ -207,36 +216,30 @@ public class Publication extends AbstractSolrDocument {
     @Indexed
     private List<String> researchAreasIds;
 
-    @Indexed
-    private String uri;
-
-    @Indexed(type = "string")
-    private String volume;
-
     public Publication() {
-        this.authorships = new ArrayList<String>();
-        this.authorshipIds = new ArrayList<String>();
-        this.contacts = new ArrayList<String>();
-        this.contactEmails = new ArrayList<String>();
-        this.contactIds = new ArrayList<String>();
-        this.contactPhones = new ArrayList<String>();
+        this.authors = new ArrayList<String>();
+        this.authorIds = new ArrayList<String>();
+        this.publishers = new ArrayList<String>();
+        this.publisherEmails = new ArrayList<String>();
+        this.publisherPhones = new ArrayList<String>();
+        this.publisherIds = new ArrayList<String>();
         this.etdChairs = new ArrayList<String>();
         this.etdChairEmails = new ArrayList<String>();
-        this.etdChairIds = new ArrayList<String>();
         this.etdChairPhones = new ArrayList<String>();
-        this.dateTimeValue = new ArrayList<String>();
+        this.etdChairIds = new ArrayList<String>();
+        this.contacts = new ArrayList<String>();
+        this.contactEmails = new ArrayList<String>();
+        this.contactPhones = new ArrayList<String>();
+        this.contactIds = new ArrayList<String>();
         this.grant = new ArrayList<String>();
         this.grantIds = new ArrayList<String>();
         this.journal = new ArrayList<String>();
         this.journalIds = new ArrayList<String>();
         this.journalIssn = new ArrayList<String>();
-        this.publishers = new ArrayList<String>();
-        this.publisherEmails = new ArrayList<String>();
-        this.publisherIds = new ArrayList<String>();
-        this.publisherPhones = new ArrayList<String>();
-        this.researchAreas = new ArrayList<String>();
-        this.researchAreasIds = new ArrayList<String>();
         this.subjectAreas = new ArrayList<String>();
         this.subjectAreasIds = new ArrayList<String>();
+        this.researchAreas = new ArrayList<String>();
+        this.researchAreasIds = new ArrayList<String>();
     }
+
 }
--- middleware/src/main/java/edu/tamu/scholars/middleware/harvest/service/PersonHarvestService.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/harvest/service/PersonHarvestService.java
@@ -8,4 +8,9 @@ import edu.tamu.scholars.middleware.discovery.service.PersonIndexService;
 @Service
 public class PersonHarvestService extends AbstractHarvestService<Person, PersonIndexService> {
 
+	@Override
+	public void harvest() {
+
+	}
+
 }
