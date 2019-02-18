f660ce5e27be30959e82f0eb621c2c0602f75aeb
Kevin Day <kday@library.tamu.edu>
2019-02-18 17:23:40 -0600

    Discover Relationship, focusing on Grant

--- middleware/src/main/java/edu/tamu/scholars/middleware/discovery/model/Relationship.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/discovery/model/Relationship.java
@@ -4,10 +4,12 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
 
 import java.util.List;
 
+import org.apache.solr.client.solrj.beans.Field;
 import org.springframework.data.solr.core.mapping.Indexed;
 import org.springframework.data.solr.core.mapping.SolrDocument;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import edu.tamu.scholars.middleware.discovery.annotation.CollectionSource;
 import edu.tamu.scholars.middleware.discovery.annotation.PropertySource;
@@ -33,12 +35,18 @@ public class Relationship extends AbstractSolrDocument {
     @PropertySource(template = "relationship/thumbnail", key = "relationship.thumbnail")
     private String thumbnail;
 
+    @Field("abstract")
+    @Indexed("abstract")
+    @JsonProperty("abstract")
+    @PropertySource(template = "relationship/abstract", key = "relationship.abstract")
+    private String abstractText;
+
     @Indexed
     @PropertySource(template = "relationship/description", key = "relationship.description")
     private String description;
 
     @Indexed
-    @PropertySource(template = "relationship/receiptOf", key = "relationship.receiptOf.label", id = "receiptOfId")
+    @PropertySource(template = "relationship/receiptOf", key = "relationship.receiptOf.name", id = "receiptOfId")
     private List<String> receiptOf;
 
     @Indexed
@@ -49,7 +57,7 @@ public class Relationship extends AbstractSolrDocument {
     private List<String> receiptOfId;
 
     @Indexed
-    @PropertySource(template = "relationship/awardOrHonorFor", key = "relationship.awardOrHonorFor.label", id = "awardOrHonorForId")
+    @PropertySource(template = "relationship/awardOrHonorFor", key = "relationship.awardOrHonorFor.name", id = "awardOrHonorForId")
     private List<String> awardOrHonorFor;
 
     @Indexed
@@ -60,7 +68,7 @@ public class Relationship extends AbstractSolrDocument {
     private List<String> awardOrHonorForId;
 
     @Indexed
-    @PropertySource(template = "relationship/awardConferredBy", key = "relationship.awardConferredBy.label", id = "awardConferredById")
+    @PropertySource(template = "relationship/awardConferredBy", key = "relationship.awardConferredBy.name", id = "awardConferredById")
     private List<String> awardConferredBy;
 
     @Indexed
@@ -70,6 +78,121 @@ public class Relationship extends AbstractSolrDocument {
     @Indexed
     private List<String> awardConferredById;
 
+    @Indexed
+    @PropertySource(template = "relationship/awardedBy", key = "relationship.awardedBy.name", id = "awardedById")
+    private List<String> awardedBy;
+
+    @Indexed
+    @PropertySource(template = "relationship/awardedByType", key = "relationship.awardedBy.type", parse = true)
+    private List<String> awardedByType;
+
+    @Indexed
+    private List<String> awardedById;
+
+    @Indexed
+    @PropertySource(template = "relationship/grantSubcontractedThrough", key = "relationship.grantSubcontractedThrough.name", id = "grantSubcontractedThroughId")
+    private List<String> grantSubcontractedThrough;
+
+    @Indexed
+    @PropertySource(template = "relationship/grantSubcontractedThroughType", key = "relationship.grantSubcontractedThrough.type", parse = true)
+    private List<String> grantSubcontractedThroughType;
+
+    @Indexed
+    private List<String> grantSubcontractedThroughId;
+
+    @Indexed
+    @PropertySource(template = "relationship/administeredBy", key = "relationship.administeredBy.name", id = "administeredById")
+    private List<String> administeredBy;
+
+    @Indexed
+    @PropertySource(template = "relationship/administeredByType", key = "relationship.administeredBy.type", parse = true)
+    private List<String> administeredByType;
+
+    @Indexed
+    private List<String> administeredById;
+
+    @Indexed
+    @PropertySource(template = "relationship/geographicFocus", key = "relationship.geographicFocus.name", id = "geographicFocusId")
+    private List<String> geographicFocus;
+
+    @Indexed
+    @PropertySource(template = "relationship/geographicFocusType", key = "relationship.geographicFocus.type", parse = true)
+    private List<String> geographicFocusType;
+
+    @Indexed
+    private List<String> geographicFocusId;
+
+    @Indexed
+    @PropertySource(template = "relationship/subGrant", key = "relationship.subGrant.name", id = "subGrantId")
+    private List<String> subGrant;
+
+    @Indexed
+    @PropertySource(template = "relationship/subGrantType", key = "relationship.subGrant.type", parse = true)
+    private List<String> subGrantType;
+
+    @Indexed
+    private List<String> subGrantId;
+
+    @Indexed
+    @PropertySource(template = "relationship/subGrantOf", key = "relationship.subGrantOf.name", id = "subGrantOfId")
+    private List<String> subGrantOf;
+
+    @Indexed
+    @PropertySource(template = "relationship/subGrantOfType", key = "relationship.subGrantOf.type", parse = true)
+    private List<String> subGrantOfType;
+
+    @Indexed
+    private List<String> subGrantOfId;
+
+    @Indexed
+    @PropertySource(template = "relationship/providesFundingFor", key = "relationship.providesFundingFor.name", id = "providesFundingForId")
+    private List<String> providesFundingFor;
+
+    @Indexed
+    @PropertySource(template = "relationship/providesFundingForType", key = "relationship.providesFundingFor.type", parse = true)
+    private List<String> providesFundingForType;
+
+    @Indexed
+    private List<String> providesFundingForId;
+
+    @Indexed
+    @PropertySource(template = "relationship/totalAwardAmount", key = "relationship.totalAwardAmount")
+    private String totalAwardAmount;
+
+    @Indexed
+    @PropertySource(template = "relationship/directCosts", key = "relationship.directCosts")
+    private String directCosts;
+
+    @Indexed
+    @PropertySource(template = "relationship/sponsorAwardId", key = "relationship.sponsorAwardId")
+    private String sponsorAwardId;
+
+    @Indexed
+    @PropertySource(template = "relationship/localAwardId", key = "relationship.localAwardId")
+    private String localAwardId;
+
+    @Indexed
+    @PropertySource(template = "relationship/contributor", key = "relationship.contributor.name", id = "contributorId")
+    private List<String> contributor;
+
+    @Indexed
+    @PropertySource(template = "relationship/contributor", key = "relationship.contributor.type", parse = true)
+    private List<String> contributorType;
+
+    @Indexed
+    private List<String> contributorId;
+
+    @Indexed
+    @PropertySource(template = "relationship/supportedPublicationOrOtherWork", key = "relationship.supportedPublicationOrOtherWork.name", id = "supportedPublicationOrOtherWorkId")
+    private List<String> supportedPublicationOrOtherWork;
+
+    @Indexed
+    @PropertySource(template = "relationship/supportedPublicationOrOtherWork", key = "relationship.supportedPublicationOrOtherWork.type", parse = true)
+    private List<String> supportedPublicationOrOtherWorkType;
+
+    @Indexed
+    private List<String> supportedPublicationOrOtherWorkId;
+
     @Indexed(type = "pdate")
     @PropertySource(template = "relationship/dateTimeIntervalStart", key = "relationship.dateTimeInterval.start")
     private List<String> dateTimeIntervalStart;
@@ -78,34 +201,45 @@ public class Relationship extends AbstractSolrDocument {
     @PropertySource(template = "relationship/dateTimeIntervalEnd", key = "relationship.dateTimeInterval.end")
     private List<String> dateTimeIntervalEnd;
 
+    @Indexed
+    @PropertySource(template = "relationship/subjectArea", key = "relationship.subjectArea.name", id = "subjectAreaId")
+    private List<String> subjectArea;
+
+    @Indexed
+    @PropertySource(template = "relationship/subjectAreaType", key = "relationship.subjectArea.type", parse = true)
+    private List<String> subjectAreaType;
+
+    @Indexed
+    private List<String> subjectAreaId;
+
     @Indexed(type = "pdate")
     @PropertySource(template = "relationship/yearAwarded", key = "relationship.yearAwarded")
     private List<String> yearAwarded;
 
     @Indexed
-    @PropertySource(template = "relationship/sameAs", key = "relationship.sameAs.label", id = "sameAsId")
+    @PropertySource(template = "relationship/sameAs", key = "relationship.sameAs.name", id = "sameAsId")
     private List<String> sameAs;
 
     @Indexed
-    @PropertySource(template = "relationship/sameAs", key = "relationship.sameAs.type")
+    @PropertySource(template = "relationship/sameAs", key = "relationship.sameAs.type", parse = true)
     private List<String> sameAsType;
 
     @Indexed
     private List<String> sameAsId;
 
     @Indexed
-    @PropertySource(template = "relationship/inheresIn", key = "relationship.inheresIn.label", id = "inheresInId")
+    @PropertySource(template = "relationship/inheresIn", key = "relationship.inheresIn.name", id = "inheresInId")
     private List<String> inheresIn;
 
     @Indexed
-    @PropertySource(template = "relationship/inheresIn", key = "relationship.inheresIn.type")
+    @PropertySource(template = "relationship/inheresIn", key = "relationship.inheresIn.type", parse = true)
     private List<String> inheresInType;
 
     @Indexed
     private List<String> inheresInId;
 
     @Indexed
-    @PropertySource(template = "relationship/isSpecifiedOutputOf", key = "relationship.isSpecifiedOutputOf.label", id = "isSpecifiedOutputOfId")
+    @PropertySource(template = "relationship/isSpecifiedOutputOf", key = "relationship.isSpecifiedOutputOf.name", id = "isSpecifiedOutputOfId")
     private List<String> specifiedOutputOf;
 
     @Indexed
@@ -116,7 +250,7 @@ public class Relationship extends AbstractSolrDocument {
     private List<String> isSpecifiedOutputOfId;
 
     @Indexed
-    @PropertySource(template = "relationship/outputOf", key = "relationship.outputOf.label", id = "outputOfId")
+    @PropertySource(template = "relationship/outputOf", key = "relationship.outputOf.name", id = "outputOfId")
     private List<String> outputOf;
 
     @Indexed
@@ -127,7 +261,7 @@ public class Relationship extends AbstractSolrDocument {
     private List<String> outputOfId;
 
     @Indexed
-    @PropertySource(template = "relationship/participatesIn", key = "relationship.participatesIn.label", id = "participatesInId")
+    @PropertySource(template = "relationship/participatesIn", key = "relationship.participatesIn.name", id = "participatesInId")
     private List<String> participatesIn;
 
     @Indexed
@@ -175,6 +309,14 @@ public class Relationship extends AbstractSolrDocument {
 
     public void setThumbnail(String thumbnail) {
         this.thumbnail = thumbnail;
+    }
+
+    public String getAbstractText() {
+        return abstractText;
+    }
+
+    public void setAbstractText(String abstractText) {
+        this.abstractText = abstractText;
     }
 
     public String getDescription() {
@@ -257,6 +399,254 @@ public class Relationship extends AbstractSolrDocument {
         this.awardConferredById = awardConferredById;
     }
 
+    public List<String> getAwardedBy() {
+        return awardedBy;
+    }
+
+    public void setAwardedBy(List<String> awardedBy) {
+        this.awardedBy = awardedBy;
+    }
+
+    public List<String> getAwardedByType() {
+        return awardedByType;
+    }
+
+    public void setAwardedByType(List<String> awardedByType) {
+        this.awardedByType = awardedByType;
+    }
+
+    public List<String> getAwardedById() {
+        return awardedById;
+    }
+
+    public void setAwardedById(List<String> awardedById) {
+        this.awardedById = awardedById;
+    }
+
+    public List<String> getGrantSubcontractedThrough() {
+        return grantSubcontractedThrough;
+    }
+
+    public void setGrantSubcontractedThrough(List<String> grantSubcontractedThrough) {
+        this.grantSubcontractedThrough = grantSubcontractedThrough;
+    }
+
+    public List<String> getGrantSubcontractedThroughType() {
+        return grantSubcontractedThroughType;
+    }
+
+    public void setGrantSubcontractedThroughType(List<String> grantSubcontractedThroughType) {
+        this.grantSubcontractedThroughType = grantSubcontractedThroughType;
+    }
+
+    public List<String> getGrantSubcontractedThroughId() {
+        return grantSubcontractedThroughId;
+    }
+
+    public void setGrantSubcontractedThroughId(List<String> grantSubcontractedThroughId) {
+        this.grantSubcontractedThroughId = grantSubcontractedThroughId;
+    }
+
+    public List<String> getAdministeredBy() {
+        return administeredBy;
+    }
+
+    public void setAdministeredBy(List<String> administeredBy) {
+        this.administeredBy = administeredBy;
+    }
+
+    public List<String> getAdministeredByType() {
+        return administeredByType;
+    }
+
+    public void setAdministeredByType(List<String> administeredByType) {
+        this.administeredByType = administeredByType;
+    }
+
+    public List<String> getAdministeredById() {
+        return administeredById;
+    }
+
+    public void setAdministeredById(List<String> administeredById) {
+        this.administeredById = administeredById;
+    }
+
+    public List<String> getGeographicFocus() {
+        return geographicFocus;
+    }
+
+    public void setGeographicFocus(List<String> geographicFocus) {
+        this.geographicFocus = geographicFocus;
+    }
+
+    public List<String> getGeographicFocusType() {
+        return geographicFocusType;
+    }
+
+    public void setGeographicFocusType(List<String> geographicFocusType) {
+        this.geographicFocusType = geographicFocusType;
+    }
+
+    public List<String> getGeographicFocusId() {
+        return geographicFocusId;
+    }
+
+    public void setGeographicFocusId(List<String> geographicFocusId) {
+        this.geographicFocusId = geographicFocusId;
+    }
+
+    public List<String> getSubGrant() {
+        return subGrant;
+    }
+
+    public void setSubGrant(List<String> subGrant) {
+        this.subGrant = subGrant;
+    }
+
+    public List<String> getSubGrantType() {
+        return subGrantType;
+    }
+
+    public void setSubGrantType(List<String> subGrantType) {
+        this.subGrantType = subGrantType;
+    }
+
+    public List<String> getSubGrantId() {
+        return subGrantId;
+    }
+
+    public void setSubGrantId(List<String> subGrantId) {
+        this.subGrantId = subGrantId;
+    }
+
+    public List<String> getSubGrantOf() {
+        return subGrantOf;
+    }
+
+    public void setSubGrantOf(List<String> subGrantOf) {
+        this.subGrantOf = subGrantOf;
+    }
+
+    public List<String> getSubGrantOfType() {
+        return subGrantOfType;
+    }
+
+    public void setSubGrantOfType(List<String> subGrantOfType) {
+        this.subGrantOfType = subGrantOfType;
+    }
+
+    public List<String> getSubGrantOfId() {
+        return subGrantOfId;
+    }
+
+    public void setSubGrantOfId(List<String> subGrantOfId) {
+        this.subGrantOfId = subGrantOfId;
+    }
+
+    public List<String> getProvidesFundingFor() {
+        return providesFundingFor;
+    }
+
+    public void setProvidesFundingFor(List<String> providesFundingFor) {
+        this.providesFundingFor = providesFundingFor;
+    }
+
+    public List<String> getProvidesFundingForType() {
+        return providesFundingForType;
+    }
+
+    public void setProvidesFundingForType(List<String> providesFundingForType) {
+        this.providesFundingForType = providesFundingForType;
+    }
+
+    public List<String> getProvidesFundingForId() {
+        return providesFundingForId;
+    }
+
+    public void setProvidesFundingForId(List<String> providesFundingForId) {
+        this.providesFundingForId = providesFundingForId;
+    }
+
+    public String getTotalAwardAmount() {
+        return totalAwardAmount;
+    }
+
+    public void setTotalAwardAmount(String totalAwardAmount) {
+        this.totalAwardAmount = totalAwardAmount;
+    }
+
+    public String getDirectCosts() {
+        return directCosts;
+    }
+
+    public void setDirectCosts(String directCosts) {
+        this.directCosts = directCosts;
+    }
+
+    public String getSponsorAwardId() {
+        return sponsorAwardId;
+    }
+
+    public void setSponsorAwardId(String sponsorAwardId) {
+        this.sponsorAwardId = sponsorAwardId;
+    }
+
+    public String getLocalAwardId() {
+        return localAwardId;
+    }
+
+    public void setLocalAwardId(String localAwardId) {
+        this.localAwardId = localAwardId;
+    }
+
+    public List<String> getContributor() {
+        return contributor;
+    }
+
+    public void setContributor(List<String> contributor) {
+        this.contributor = contributor;
+    }
+
+    public List<String> getContributorType() {
+        return contributorType;
+    }
+
+    public void setContributorType(List<String> contributorType) {
+        this.contributorType = contributorType;
+    }
+
+    public List<String> getContributorId() {
+        return contributorId;
+    }
+
+    public void setContributorId(List<String> contributorId) {
+        this.contributorId = contributorId;
+    }
+
+    public List<String> getSupportedPublicationOrOtherWork() {
+        return supportedPublicationOrOtherWork;
+    }
+
+    public void setSupportedPublicationOrOtherWork(List<String> supportedPublicationOrOtherWork) {
+        this.supportedPublicationOrOtherWork = supportedPublicationOrOtherWork;
+    }
+
+    public List<String> getSupportedPublicationOrOtherWorkType() {
+        return supportedPublicationOrOtherWorkType;
+    }
+
+    public void setSupportedPublicationOrOtherWorkType(List<String> supportedPublicationOrOtherWorkType) {
+        this.supportedPublicationOrOtherWorkType = supportedPublicationOrOtherWorkType;
+    }
+
+    public List<String> getSupportedPublicationOrOtherWorkId() {
+        return supportedPublicationOrOtherWorkId;
+    }
+
+    public void setSupportedPublicationOrOtherWorkId(List<String> supportedPublicationOrOtherWorkId) {
+        this.supportedPublicationOrOtherWorkId = supportedPublicationOrOtherWorkId;
+    }
+
     public List<String> getDateTimeIntervalStart() {
         return dateTimeIntervalStart;
     }
@@ -271,6 +661,30 @@ public class Relationship extends AbstractSolrDocument {
 
     public void setDateTimeIntervalEnd(List<String> dateTimeIntervalEnd) {
         this.dateTimeIntervalEnd = dateTimeIntervalEnd;
+    }
+
+    public List<String> getSubjectArea() {
+        return subjectArea;
+    }
+
+    public void setSubjectArea(List<String> subjectArea) {
+        this.subjectArea = subjectArea;
+    }
+
+    public List<String> getSubjectAreaType() {
+        return subjectAreaType;
+    }
+
+    public void setSubjectAreaType(List<String> subjectAreaType) {
+        this.subjectAreaType = subjectAreaType;
+    }
+
+    public List<String> getSubjectAreaId() {
+        return subjectAreaId;
+    }
+
+    public void setSubjectAreaId(List<String> subjectAreaId) {
+        this.subjectAreaId = subjectAreaId;
     }
 
     public List<String> getYearAwarded() {
