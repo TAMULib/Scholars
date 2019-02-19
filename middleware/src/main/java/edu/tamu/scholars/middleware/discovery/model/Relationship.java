9a550a2410c7e7491a38364213e1d3b3e2892fcc
Kevin Day <kday@library.tamu.edu>
2019-02-19 09:02:56 -0600

    Add getters and setters

--- middleware/src/main/java/edu/tamu/scholars/middleware/discovery/model/Relationship.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/discovery/model/Relationship.java
@@ -145,4 +145,268 @@ public class Relationship extends AbstractSolrDocument {
 
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getType() {
+        return type;
+    }
+
+    public void setType(List<String> type) {
+        this.type = type;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getReceiptOf() {
+        return receiptOf;
+    }
+
+    public void setReceiptOf(List<String> receiptOf) {
+        this.receiptOf = receiptOf;
+    }
+
+    public List<String> getReceiptOfType() {
+        return receiptOfType;
+    }
+
+    public void setReceiptOfType(List<String> receiptOfType) {
+        this.receiptOfType = receiptOfType;
+    }
+
+    public List<String> getReceiptOfId() {
+        return receiptOfId;
+    }
+
+    public void setReceiptOfId(List<String> receiptOfId) {
+        this.receiptOfId = receiptOfId;
+    }
+
+    public List<String> getAwardOrHonorFor() {
+        return awardOrHonorFor;
+    }
+
+    public void setAwardOrHonorFor(List<String> awardOrHonorFor) {
+        this.awardOrHonorFor = awardOrHonorFor;
+    }
+
+    public List<String> getAwardOrHonorForType() {
+        return awardOrHonorForType;
+    }
+
+    public void setAwardOrHonorForType(List<String> awardOrHonorForType) {
+        this.awardOrHonorForType = awardOrHonorForType;
+    }
+
+    public List<String> getAwardOrHonorForId() {
+        return awardOrHonorForId;
+    }
+
+    public void setAwardOrHonorForId(List<String> awardOrHonorForId) {
+        this.awardOrHonorForId = awardOrHonorForId;
+    }
+
+    public List<String> getAwardConferredBy() {
+        return awardConferredBy;
+    }
+
+    public void setAwardConferredBy(List<String> awardConferredBy) {
+        this.awardConferredBy = awardConferredBy;
+    }
+
+    public List<String> getAwardConferredByType() {
+        return awardConferredByType;
+    }
+
+    public void setAwardConferredByType(List<String> awardConferredByType) {
+        this.awardConferredByType = awardConferredByType;
+    }
+
+    public List<String> getAwardConferredById() {
+        return awardConferredById;
+    }
+
+    public void setAwardConferredById(List<String> awardConferredById) {
+        this.awardConferredById = awardConferredById;
+    }
+
+    public List<String> getDateTimeIntervalStart() {
+        return dateTimeIntervalStart;
+    }
+
+    public void setDateTimeIntervalStart(List<String> dateTimeIntervalStart) {
+        this.dateTimeIntervalStart = dateTimeIntervalStart;
+    }
+
+    public List<String> getDateTimeIntervalEnd() {
+        return dateTimeIntervalEnd;
+    }
+
+    public void setDateTimeIntervalEnd(List<String> dateTimeIntervalEnd) {
+        this.dateTimeIntervalEnd = dateTimeIntervalEnd;
+    }
+
+    public List<String> getYearAwarded() {
+        return yearAwarded;
+    }
+
+    public void setYearAwarded(List<String> yearAwarded) {
+        this.yearAwarded = yearAwarded;
+    }
+
+    public List<String> getSameAs() {
+        return sameAs;
+    }
+
+    public void setSameAs(List<String> sameAs) {
+        this.sameAs = sameAs;
+    }
+
+    public List<String> getSameAsType() {
+        return sameAsType;
+    }
+
+    public void setSameAsType(List<String> sameAsType) {
+        this.sameAsType = sameAsType;
+    }
+
+    public List<String> getSameAsId() {
+        return sameAsId;
+    }
+
+    public void setSameAsId(List<String> sameAsId) {
+        this.sameAsId = sameAsId;
+    }
+
+    public List<String> getInheresIn() {
+        return inheresIn;
+    }
+
+    public void setInheresIn(List<String> inheresIn) {
+        this.inheresIn = inheresIn;
+    }
+
+    public List<String> getInheresInType() {
+        return inheresInType;
+    }
+
+    public void setInheresInType(List<String> inheresInType) {
+        this.inheresInType = inheresInType;
+    }
+
+    public List<String> getInheresInId() {
+        return inheresInId;
+    }
+
+    public void setInheresInId(List<String> inheresInId) {
+        this.inheresInId = inheresInId;
+    }
+
+    public List<String> getSpecifiedOutputOf() {
+        return specifiedOutputOf;
+    }
+
+    public void setSpecifiedOutputOf(List<String> specifiedOutputOf) {
+        this.specifiedOutputOf = specifiedOutputOf;
+    }
+
+    public List<String> getSpecifiedOutputOfType() {
+        return specifiedOutputOfType;
+    }
+
+    public void setSpecifiedOutputOfType(List<String> specifiedOutputOfType) {
+        this.specifiedOutputOfType = specifiedOutputOfType;
+    }
+
+    public List<String> getIsSpecifiedOutputOfId() {
+        return isSpecifiedOutputOfId;
+    }
+
+    public void setIsSpecifiedOutputOfId(List<String> isSpecifiedOutputOfId) {
+        this.isSpecifiedOutputOfId = isSpecifiedOutputOfId;
+    }
+
+    public List<String> getOutputOf() {
+        return outputOf;
+    }
+
+    public void setOutputOf(List<String> outputOf) {
+        this.outputOf = outputOf;
+    }
+
+    public List<String> getOutputOfType() {
+        return outputOfType;
+    }
+
+    public void setOutputOfType(List<String> outputOfType) {
+        this.outputOfType = outputOfType;
+    }
+
+    public List<String> getOutputOfId() {
+        return outputOfId;
+    }
+
+    public void setOutputOfId(List<String> outputOfId) {
+        this.outputOfId = outputOfId;
+    }
+
+    public List<String> getParticipatesIn() {
+        return participatesIn;
+    }
+
+    public void setParticipatesIn(List<String> participatesIn) {
+        this.participatesIn = participatesIn;
+    }
+
+    public List<String> getParticipatesInType() {
+        return participatesInType;
+    }
+
+    public void setParticipatesInType(List<String> participatesInType) {
+        this.participatesInType = participatesInType;
+    }
+
+    public List<String> getParticipatesInId() {
+        return participatesInId;
+    }
+
+    public void setParticipatesInId(List<String> participatesInId) {
+        this.participatesInId = participatesInId;
+    }
+
+    public String getModTime() {
+        return modTime;
+    }
+
+    public void setModTime(String modTime) {
+        this.modTime = modTime;
+    }
+
 }
