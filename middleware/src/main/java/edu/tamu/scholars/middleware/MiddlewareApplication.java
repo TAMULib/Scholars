9d5702b66b3f5a5f83511aeab5e08d67b1e0d088
William Welling <wwelling@library.tamu.edu>
2019-02-27 22:04:36 -0600

    create json schema from model, create model from json schema

--- middleware/src/main/java/edu/tamu/scholars/middleware/MiddlewareApplication.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/MiddlewareApplication.java
@@ -4,12 +4,35 @@ import static edu.tamu.scholars.middleware.auth.AuthConstants.PASSWORD_DURATION_
 import static edu.tamu.scholars.middleware.auth.AuthConstants.PASSWORD_MAX_LENGTH;
 import static edu.tamu.scholars.middleware.auth.AuthConstants.PASSWORD_MIN_LENGTH;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.annotation.PostConstruct;
 
+import org.jsonschema2pojo.DefaultGenerationConfig;
+import org.jsonschema2pojo.GenerationConfig;
+import org.jsonschema2pojo.Jackson2Annotator;
+import org.jsonschema2pojo.SchemaGenerator;
+import org.jsonschema2pojo.SchemaMapper;
+import org.jsonschema2pojo.SchemaStore;
+import org.jsonschema2pojo.rules.RuleFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.io.Resource;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.data.solr.core.mapping.SolrDocument;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import com.sun.codemodel.JCodeModel;
 
 import edu.tamu.scholars.middleware.auth.config.AuthConfig;
 import edu.tamu.scholars.middleware.auth.config.PasswordConfig;
@@ -22,6 +45,9 @@ public class MiddlewareApplication {
     @Autowired
     private MiddlewareConfig middlewareConfig;
 
+    @Value("classpath:/")
+    public Resource classpath;
+
     public static void main(String[] args) {
         SpringApplication.run(MiddlewareApplication.class, args);
     }
@@ -33,6 +59,58 @@ public class MiddlewareApplication {
         PASSWORD_DURATION_IN_DAYS = password.getDuration();
         PASSWORD_MIN_LENGTH = password.getMinLength();
         PASSWORD_MAX_LENGTH = password.getMaxLength();
+    }
+
+    @PostConstruct
+    private void generateSchema() throws ClassNotFoundException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter());
+
+        JsonSchemaGenerator jsonSchemaGenerator = new JsonSchemaGenerator(objectMapper);
+
+        File schemaDirectory = new File(String.format("%s/schema", classpath.getFile().getAbsolutePath()));
+
+        if (!schemaDirectory.exists()) {
+            schemaDirectory.mkdir();
+        }
+
+        JCodeModel codeModel = new JCodeModel();
+
+        GenerationConfig config = new DefaultGenerationConfig() {
+            @Override
+            public boolean isGenerateBuilders() {
+                return true;
+            }
+        };
+
+        SchemaMapper schemaMapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
+
+        ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
+
+        provider.addIncludeFilter(new AnnotationTypeFilter(SolrDocument.class));
+
+        for (BeanDefinition beanDefinition : provider.findCandidateComponents("edu.tamu.scholars.middleware.discovery.model")) {
+            Class<?> clazz = Class.forName(beanDefinition.getBeanClassName());
+
+            JsonSchema jsonSchema = jsonSchemaGenerator.generateSchema(clazz);
+
+            File schema = new File(String.format("%s/%s.json", schemaDirectory.getAbsolutePath(), clazz.getSimpleName()));
+
+            objectWriter.writeValue(schema, jsonSchema);
+
+            schemaMapper.generate(codeModel, clazz.getSimpleName(), "edu.tamu.scholars.middleware.discovery.model", schema.toURI().toURL());
+        }
+
+        File sourceDirectory = new File(String.format("%s/source", classpath.getFile().getAbsolutePath()));
+
+        if (!sourceDirectory.exists()) {
+            sourceDirectory.mkdir();
+        }
+
+        codeModel.build(sourceDirectory);
+
     }
 
 }
