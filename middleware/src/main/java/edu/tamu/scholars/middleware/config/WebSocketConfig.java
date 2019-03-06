52b77e8a3217ad06acc762dcc927cefcd5bd01b3
William Welling <wwelling@library.tamu.edu>
2019-03-06 11:11:47 -0600

    add patch for spring session handshake bug

--- middleware/src/main/java/edu/tamu/scholars/middleware/config/WebSocketConfig.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/config/WebSocketConfig.java
@@ -1,12 +1,20 @@
 package edu.tamu.scholars.middleware.config;
 
+import static org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor.HTTP_SESSION_ID_ATTR_NAME;
+
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.session.Session;
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import edu.tamu.scholars.middleware.messaging.handler.CustomStompSubProtocolErrorHandler;
@@ -14,6 +22,8 @@ import edu.tamu.scholars.middleware.messaging.handler.CustomStompSubProtocolErro
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<Session> {
+
+    private static final String SPRING_SESSION_ID_ATTR_NAME = "SPRING.SESSION.ID";
 
     @Value("${ui.url:http://localhost:4200}")
     private String uiUrl;
@@ -25,7 +35,26 @@ public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfig
         registry
             .addEndpoint("/connect")
             .setAllowedOrigins(uiUrl).withSockJS()
-            .setInterceptors(new HttpSessionHandshakeInterceptor());
+            .setInterceptors(
+                new HttpSessionHandshakeInterceptor(),
+                // TODO: remove when patched in spring-session, https://github.com/spring-projects/spring-session/issues/561
+                new HandshakeInterceptor() {
+
+                    @Override
+                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                        if (attributes.containsKey(HTTP_SESSION_ID_ATTR_NAME)) {
+                            attributes.put(SPRING_SESSION_ID_ATTR_NAME, attributes.get(HTTP_SESSION_ID_ATTR_NAME));
+                        }
+                        return true;
+                    }
+
+                    @Override
+                    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+                    }
+
+                }
+            );
         // @formatter:on
     }
 
