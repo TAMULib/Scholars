9cc77b86bd50cf83eaf66565f24e92136b4ebec8
Kevin Day <kday@library.tamu.edu>
2019-03-11 09:27:58 -0500

    Add TranslateModule.forRoot to sidebar.component.spec.ts

--- ui/src/app/shared/sidebar/sidebar.component.spec.ts
+++ ui/src/app/shared/sidebar/sidebar.component.spec.ts
@@ -1,6 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { StoreModule } from '@ngrx/store';
 
 import { SharedModule } from '../shared.module';
@@ -20,6 +21,7 @@ describe('SidebarComponent', () => {
                 StoreModule.forRoot(reducers, {
                     metaReducers
                 }),
+                TranslateModule.forRoot(),
                 RouterTestingModule.withRoutes([])
             ],
             schemas: [
