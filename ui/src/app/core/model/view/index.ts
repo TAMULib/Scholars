7f5e6d06af05a9a0c3d1e71d0afe44803b95d238
William Welling <wwelling@library.tamu.edu>
2019-04-01 14:44:17 -0500

    diplay view front end models

--- ui/src/app/core/model/view/index.ts
+++ ui/src/app/core/model/view/index.ts
@@ -1,4 +1,5 @@
 export { Layout, FacetSort, Facet, Filter, CollectionView } from './collection-view';
 export { DiscoveryView } from './discovery-view';
 export { OperationKey, Index, DirectoryView } from './directory-view';
+export { ResourceView } from './resource-view';
 export { View } from './view';
