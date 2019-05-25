eafe496853d3f31f02da51718f645675f552e0c9
William Welling <wwelling@library.tamu.edu>
2019-05-24 21:24:43 -0500

    minor template fix, alert error messages

--- ui/base.js
+++ ui/base.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = Number(process.env.PORT) || 4200;
-const BASE_HREF = process.env.BASE_HREF || '/';
+let BASE_HREF = process.env.BASE_HREF || '/';
 
 if (!BASE_HREF.startsWith('/')) {
     BASE_HREF = `/${BASE_HREF}`;
